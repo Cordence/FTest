4e55395efbf5bee21a4c9ce2a5ac361597fbdfda
Cordence <57625947+Cordence@users.noreply.github.com>
2020-05-17 23:53:39 +0900

    Update web.sql

--- web.sql
+++ web.sql
@@ -1,8 +1,8 @@
 create table customer 
 (
-	id varchar(16) not null,
-	score int not null,
-	temp int not null,
-	stat int not null,
+	id varchar(16) not null,   // 사용자 아이디
+	score int not null,	   // 피부 점수	
+	temp int not null,	   // 피부 온도
+	stat int not null,	   // 피부 상태
 	time timestamp DEFAULT CURRENT_TIMESTAMP
 )	
